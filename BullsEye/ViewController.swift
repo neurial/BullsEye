595c1a5af02d91c0dce054ecc768c320269a48f3
G. Ellisberg <dev@neurial.com>
2016-08-14 02:21:44 -0600

    Fooled around
    Still all messed up with “editor placeholders” nonsense

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -20,14 +20,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func showAlert() {
-        let ggAlert = UIAlertController(title: "Hello World", message: "Making progress here...", preferredStyle: <#T##UIAlertControllerStyle#>)
-        let ggAction = UIAlertAction(title: "Awesome", style: <#T##UIAlertActionStyle#>, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
-        
-        ggAlert.addAction(ggAction)
-        
-        present(ggAlert, animated: true, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
-    }
+//    @IBAction func showAlert() {
+//        
+//        let ggAlert = UIAlertController(title: "Hello World", message: "Making progress here...", preferredStyle: <#T##UIAlertControllerStyle#>)
+//        let ggAction = UIAlertAction(title: "Awesome", style: <#T##UIAlertActionStyle#>, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+//        
+//        ggAlert.addAction(ggAction)
+//        
+//        present(ggAlert, animated: true, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+//    }
 
+    @IBAction func showAlert(_ sender: UIButton) {
+        let emAlert = UIAlertController(title: "Hello", message: "Here is a special message for you", preferredStyle: <#T##UIAlertControllerStyle#>)
+        let emAction = UIAlertAction(title: "Got it", style: <#T##UIAlertActionStyle#>, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+        emAlert.addAction(emAction)
+        present(emAlert, animated:true, completion: nil)
+        
+    }
 }
 
